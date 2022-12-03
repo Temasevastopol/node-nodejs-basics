2d7edefaffe74289dc00ea83efd136757e1e5228
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-04 00:39:04 +0300

    feat: solution#rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
-const rename = async () => {
+const fs = require('fs')
+const path = require('path')
+
+const pathFileBefore = path.join(__dirname, 'files', 'wrongFilename.txt')
+const pathFileAfter = path.join(__dirname, 'files', 'properFilename.md')
+
+const rename = async () => {      
+    try{        
+       fs.rename(pathFileBefore, pathFileAfter, (err, stats)=>{          
+            if(err) throw new Error('FS operation failed');
+        })
+       
+    } catch(err){
+        console.log(err)
+    }
     // Write your code here 
 };
 
-await rename();
+rename();
