213b1ca6b4e2c79404d4c5eda6aa22de635e69f3
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-03 21:46:59 +0300

    feat: solution #create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+
+const pathFile = path.join(__dirname, 'files', 'fresh.txt')
+
 const create = async () => {
-    // Write your code here 
+    try{
+         fs.stat(pathFile, (err, stat)=>{            
+            if(err === null){
+                throw new Error('FS operation failed')
+            } 
+
+            else if(err.code === 'ENOENT'){                
+               fs.writeFile(pathFile, 'I am fresh and young', (err)=>{
+                    if(err) throw err;
+                })
+            }
+        })
+    } catch(err){
+        console.log(err)
+    }    
+    // Write your code here e
 };
 
-await create();
+create()
