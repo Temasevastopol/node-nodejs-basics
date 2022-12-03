91eb27f29f9a1d5b7b20b328431245b53d226da8
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-03 22:29:25 +0300

    feat:solution#1

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,4 +1,25 @@
-const copy = async () => {
+const fs = require('fs')
+const path = require('path')
+
+const pathFolder = path.join(__dirname, 'files_copy')
+
+const copy = async () => {    
+    try{
+         fs.stat(pathFolder,(err, stat)=>{            
+            if(err === null){                
+                throw new Error('FS operation failed')
+            }
+            else if(err.code === 'ENOENT'){
+                fs.mkdir(pathFolder, (err)=>{
+                    if(err) throw err;
+                })
+                console.log('Folder was create')
+            }
+            
+        })           
+    } catch(err){
+        console.log(err)
+    }
     // Write your code here 
 };
 
