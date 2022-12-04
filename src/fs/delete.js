186c63d1b9b7044fefca0054fc914b8c9fc66331
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-04 19:55:21 +0300

    feat:solution#delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+const fs = require('fs');
+const path = require('path')
+
+const targetFile = path.join(__dirname, 'files', 'fileToRemove.txt')
+
 const remove = async () => {
+    try{
+        fs.unlink(targetFile, (err)=>{
+            if(err) throw new Error('FS operation failed')
+        })
+    } catch(err){
+        console.log(err)
+    }
+    
     // Write your code here 
 };
 
-await remove();
+remove();
