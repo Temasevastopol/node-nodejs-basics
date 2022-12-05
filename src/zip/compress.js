4a91f8b696c613ab1bfcfc94d4bede7f1323a04f
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-05 21:50:04 +0300

    feat: solution #compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
-const compress = async () => {
+const { createReadStream, createWriteStream } = require('fs');
+const { pipeline } = require('stream');
+const { promisify } = require('util');
+const { createGzip } = require('zlib');
+const path = require('path')
+const pipe = promisify(pipeline);
+
+const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const fileCompressPath = path.join(__dirname, 'files', 'archiv.gz')
+
+const compress = async (input, output) => {
+
+    try{
+        const gzip = createGzip();
+        const source  = createReadStream(input);
+        const destination = createWriteStream(output);
+        await pipe(source, gzip, destination);
+    } catch(err){
+        console.log(err);
+        process.exitCode = 1; 
+    }
+
     // Write your code here 
 };
 
-await compress();
+ compress(filePath, fileCompressPath);
