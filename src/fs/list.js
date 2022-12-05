d1cf3d93b5a13b2b6f3bec839874bb9ae85d3b60
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-05 20:42:17 +0300

    feat: solution #list and #read

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+const fs = require('fs');
+const path = require('path')
+
+
 const list = async () => {
+    try{
+        fs.readdir(path.join(__dirname, 'files'), (err, files)=>{
+            if(err) throw new Error('FS operation failed');
+            console.log(files)
+        })
+    }catch(err){
+        console.log(err)
+    }
     // Write your code here 
 };
 
-await list();
+ list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+const path = require('path')
+
+const targetFile = path.join(__dirname, 'files', 'fileToRead.txt')
+
+
 const read = async () => {
+    try{
+        fs.readFile(targetFile, 'utf-8', (err,data)=>{
+            if(err) throw new Error('FS operation failed');
+            console.log(data);
+        })
+    }catch(err){
+        console.log(err)
+    }
     // Write your code here 
 };
 
-await read();
+read();
