acc1ec39deea68597686f6f3b7c3d05c53bf4703
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-05 22:23:08 +0300

    feat: solution #hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
-const calculateHash = async () => {
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+
+const calculateHash = (alg) => {
+
+    try{
+        const algoritm = alg;
+        const shaResult = crypto.createHash(algoritm);
+        const stream = fs.ReadStream(filePath)
+
+        stream.on('data', (data)=>{            
+            shaResult.update(data)
+        })
+
+        stream.on('end', ()=>{
+            const hash = shaResult.digest('hex')
+            console.log('hash - ' + hash + '  for ' + filePath)
+        })
+    } catch(err){
+        console.log(err)
+    }
+    
+
     // Write your code here 
 };
 
-await calculateHash();
+calculateHash('sha256');
