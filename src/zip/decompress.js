97c77c798f0854020440b3b7e517b347a5561db8
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-05 21:57:21 +0300

    feat: solution #decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
-const decompress = async () => {
+const fs = require('fs');
+const zlib = require('zlib');
+const unzip = zlib.createUnzip()
+const path = require('path');
+
+const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const fileCompressPath = path.join(__dirname, 'files', 'archiv.gz');
+
+const decompress = () => {
+    try{
+        const input = fs.createReadStream(fileCompressPath);
+        const output = fs.createWriteStream(filePath);
+        input.pipe(unzip).pipe(output);
+    }catch(err){
+        console.log(err)
+    }
     // Write your code here 
 };
-
-await decompress();
+decompress();
