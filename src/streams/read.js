1d2137a72de8bb7dfba1f17433ae0618b185c757
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-05 22:42:33 +0300

    feat: solution #stream_read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
-const read = async () => {
+const fs = require('fs')
+const path = require('path');
+const precess = require('process')
+
+const  filePath = path.join(__dirname, 'files', 'fileToRead.txt')
+
+const read = (path) => {
+    try{
+        const stream = fs.createReadStream(path, 'utf-8');
+
+        let data = '';
+
+        stream.on('data', chunk => data += chunk); 
+        stream.on('end', ()=>process.stdout.write(data));
+        stream.on('error', error => process.stdout.write('error ' + error.message));
+    }catch(err){
+        process.stdoun.write(err)
+    }
+    
     // Write your code here 
 };
 
-await read();
+read(filePath);
